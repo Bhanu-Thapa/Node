871b5dff8119dbade84ce45e6261dfc5f93dd63e
BhanuThapa <bhanuthapa252@gmail.com>
2022-12-27 23:54:45 +0530

    create and export own Module

---
+++ importExport/index.js
@@ -0,0 +1,18 @@
+// const oper = require('./oper');
+// console.log(oper);
+// console.log(oper.add(5, 5));
+// console.log(oper.sub(10, 5));
+// console.log(kuchv);
+
+// const { add1, sub1, mult1 } = require('./oper'); // object destructuring
+
+// console.log(add1(5, 5));
+// console.log(sub1(10, 5));
+// console.log(mult1(10, 5));
+
+const { add, sub, mult, name } = require('./oper'); // object destructuring
+
+console.log(add(5, 5));
+console.log(sub(10, 5));
+console.log(mult(10, 5));
+console.log(name);
---
+++ importExport/oper.js
@@ -0,0 +1,19 @@
+const add = (a, b) => {
+  return a + b;
+};
+
+const sub = (a, b) => {
+  return a - b;
+};
+
+const mult = (a, b) => {
+  return a * b;
+};
+
+const name = 'Bhanu';
+
+// module.exports.add1 = add;
+// module.exports.sub1 = sub;
+// module.exports.multi1 = multi;
+
+module.exports = { add, sub, mult, name };
