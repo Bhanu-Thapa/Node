8a9235a82d71ab17644f854e47fc2f7e617a834c
BhanuThapa <bhanuthapa252@gmail.com>
2023-01-05 22:11:06 +0530

    NPM Module

--- npmMod/index.js
+++ npmMod/index.js
@@ -1,3 +1,8 @@
 const chalk = require('chalk');
+const validator = require('validator');
 
-console.log(chalk.blue('hello world'));
+console.log(chalk.green.underline.inverse('success'));
+
+const res = validator.isEmail('bhanuthapa@gmail.com');
+console.log(res);
+console.log(res ? chalk.green.inverse(res) : chalk.red.inverse(res));
